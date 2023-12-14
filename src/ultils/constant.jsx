c28efd0cc4039997b07a70d62754aec5ef04356b
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-12-14 21:21:23 +0700

    Fix Merch code v2

--- src/ultils/constant.jsx
+++ src/ultils/constant.jsx
@@ -1,4 +1,10 @@
-import { BsFilePerson, BsPostcard } from "react-icons/bs"
+import { BsFilePerson, BsPostcard, BsDoorOpenFill,
+  BsFillPieChartFill,
+  BsFillHouseGearFill,
+  BsDropletHalf,
+  BsFillLightningFill,
+  BsCashCoin,
+  BsStack, } from "react-icons/bs"
 import {
   AiFillDashboard,
   AiFillDollarCircle,
@@ -457,5 +463,49 @@ export const statuses = [
     name: "Từ chối",
     label: "Từ chối",
     value: "REJECTED",
+  },
+]
+export const superAdminSidebar = [
+  {
+    id: 1,
+    name: "Thống kê",
+    path: `${path.DASHBOARD}`,
+    icon: <BsFillPieChartFill size={20} />,
+    type: "SINGLE",
+  },
+  {
+    id: 2,
+    name: "Quản lý Nhà",
+    icon: <BsFillHouseGearFill size={20} />,
+    type: "SINGLE",
+    path: `${path.MANAGER_HOUSE}`,
+  },
+  {
+    id: 3,
+    name: "Quản lý dịch vụ phòng",
+    icon: <BsStack size={20} />,
+    type: "SINGLE",
+    path: `${path.MANAGER_SERVICE}`,
+  },
+  {
+    id: 4,
+    name: "Quản lý điện & nước",
+    icon: <BsDropletHalf size={20} />,
+    type: "SINGLE",
+    path: `${path.MANAGER_ELECTRIC_WATER}`,
+  },
+  {
+    id: 5,
+    name: "Quản lý tiền phòng",
+    icon: <BsCashCoin size={20} />,
+    type: "SINGLE",
+    path: `${path.MANAGER_PAYMENT}`,
+  },
+  {
+    id: 6,
+    name: "Tới Homepage",
+    path: `/`,
+    icon: <RiShareForwardFill size={20} />,
+    type: "SINGLE",
   },
 ]
