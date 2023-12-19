a796fa8add6dc6dca2ac8a9aa900d867bea96762
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-12-20 02:27:09 +0700

    Fix 2

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-  baseURL: "https://nhatro-spring-app-20231220005522.azuremicroservices.io/api/v1"
+  baseURL: "https://nhatro-spring-app-20231220005522.azuremicroservices.io/api/v1",
 })
 instance.interceptors.request.use(
   function (config) {
